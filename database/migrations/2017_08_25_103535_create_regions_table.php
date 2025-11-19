dfa8128cedcdf11d3c860cd10ae557615df96eaf
jrglaber <edivaldo@tmax.com.br>
2025-11-19 10:11:32 -0300

    Change primary key from increments to intege

--- database/migrations/2017_08_25_103535_create_regions_table.php
+++ database/migrations/2017_08_25_103535_create_regions_table.php
@@ -14,7 +14,7 @@ class CreateRegionsTable extends Migration
     public function up()
     {
         Schema::create('regions', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id')->primary();
             $table->string('title');
             $table->string('slug');
         });
