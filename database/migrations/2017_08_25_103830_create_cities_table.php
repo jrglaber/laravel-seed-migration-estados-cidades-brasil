19cf07099069ed34df1f841cbca9d7c4e787242d
jrglaber <edivaldo@tmax.com.br>
2025-11-19 10:12:13 -0300

    Modify cities table primary key type
    Change primary key from increments to integer type.

--- database/migrations/2017_08_25_103830_create_cities_table.php
+++ database/migrations/2017_08_25_103830_create_cities_table.php
@@ -14,7 +14,7 @@ class CreateCitiesTable extends Migration
     public function up()
     {
         Schema::create('cities', function (Blueprint $table) {
-            $table->increments('id');
+            $table->integer('id')->primary();
             $table->integer('state_id')->unsigned();
             $table->string('title');
             $table->integer('iso');
